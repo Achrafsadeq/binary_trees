289a534e2bfc697818e152aee16f4d153ff9c6f0
Achrafsadeq <achrafsadeqq@gmail.com>
2024-12-03 09:04:59 -0800

    Measure the balance factor of a binary tree.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,36 +2,38 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: Pointer to the root node of the tree
  *
- * Return: The height of the tree. If tree is NULL, return 0.
+ * Return: Height of the tree, 0 if tree is NULL
  */
-static size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	/* Calculate the height of left and right subtrees recursively */
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (left_height > right_height ? left_height : right_height);
 }
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
+ * @tree: Pointer to the root node of the tree
  *
- * Return: The balance factor of the tree. If tree is NULL, return 0.
+ * Return: Balance factor, 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	/* Calculate the height of the left and right subtrees */
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = tree->left ? binary_tree_height(tree->left) : -1;
+	right_height = tree->right ? binary_tree_height(tree->right) : -1;
 
 	return (left_height - right_height);
 }
